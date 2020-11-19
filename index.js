f2bcbac19db6a6117e581da7b34839edf4c6601a
cisco <sovisrushain@gmail.com>
2020-11-19 09:23:38 +0530

    Implement add todo part.

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-document.querySelector('#btn').addEventListener('click', function(){
-    var input = document.querySelector('input').value;
-    console.log(input);
+
+$('#btn').click(function(){
+    var input = $('#input').val();
+    $('#items').append('<li class="list-group-item"><div class="d-flex justify-content-between"><p>'+input+'</p><button id="btn-delete" class="btn btn-outline-dark btn-sm m-2">Delete</button></div></li>');
+    ; 
+    $('#input').val('');
+    $('ul button').click(function(){
+         $(this).parents('li').remove();
+    })
 })
+
+
+
