d3b4c8caa19c3370ddc2a50654743460227ed32f
cisco <sovisrushain@gmail.com>
2020-11-19 15:38:20 +0530

    Remove all and add adding items and search functionality with JS ES6.

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 
-$('#btn').click(function(){
-    var input = $('#input').val();
-    $('#items').append('<li class="list-group-item"><div class="d-flex justify-content-between"><p>'+input+'</p><button id="btn-delete" class="btn btn-outline-dark btn-sm m-2">Delete</button></div></li>');
-    ; 
+const todo_list = [];
+
+$('#btn').click(() => {
+    todo_list.push($('#input').val());
+    showList(todo_list);
     $('#input').val('');
-    $('ul button').click(function(){
-         $(this).parents('li').remove();
-    })
 })
+
+ showList = todo_list =>{
+    $('#items').children().remove();
+    todo_list.map(item => $('#items').append('<li class="list-group-item"><div class="d-flex justify-content-between"><p>'+item+'</p><button id="btn-delete" class="btn btn-outline-dark btn-sm m-2">Delete</button></div></li>'))
+};
+
+$('#search-item').keyup((e) => {
+    const filtered_list = [];
+    let input = e.target.value;
+    todo_list.map(item => item.includes(input) ? filtered_list.push(item) : null);
+    showFilteredList(filtered_list);
+})
+
+ showFilteredList = filtered_list => {
+    $('#items').children().remove();
+    filtered_list.map(item => $('#items').append('<li class="list-group-item"><div class="d-flex justify-content-between"><p>'+item+'</p><button id="btn-delete" class="btn btn-outline-dark btn-sm m-2">Delete</button></div></li>'))
+};
+
+
+
+
 
 
 
